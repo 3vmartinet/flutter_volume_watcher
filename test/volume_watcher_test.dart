5badadc9389c991c2ff4c2635fd15b286a50aae9
Valentin Martinet <mofx71@gmail.com>
2024-04-26 16:14:01 +0200

    Update Android variant

--- test/volume_watcher_test.dart
+++ test/volume_watcher_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:volume_watcher/volume_watcher.dart';
+import 'package:flutter_volume_watcher/volume_watcher.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('volume_watcher');
